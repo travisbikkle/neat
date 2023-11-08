855b9642fddfd390f3e2317313afb1cc08fe54b1
travisbikkle <travisbikkle@proton.me>
2023-11-08 22:33:43 +0800

    delete useless files

--- js/copy.js
+++ js/copy.js
@@ -1,25 +1,8 @@
 (function () {
     'use strict';
 
-    function createTextBlob(text) {
-        return new Blob([text], {type: "text/plain"});
-    }
-
-    function createHtmlBlob(text) {
-        return new Blob([text], {type: "text/html"});
-    }
-
-    async function createImageBlob(url) {
-        const response = await fetch(url);
-        return await response.blob();
-    }
-
-    function select(element) {
-        const selection = window.getSelection();
-        const range = document.createRange();
-        range.selectNodeContents(element);
-        selection.removeAllRanges();
-        selection.addRange(range);
+    function createHtmlBlob(target) {
+        return new Blob([target], {type: "text/html"});
     }
 
     async function writeDataToClipboard(blob) {
@@ -30,48 +13,28 @@
                 });
                 await navigator.clipboard.write([item]);
             } catch (error) {
-                console.error("复制失败", error);
+                console.error("failed to copy", error);
             }
         }
     }
 
-    function copyPlainText() {
-        console.clear()
-        const selection = window.getSelection(0);
-        let selected_str = selection.toString()
-        let selected_blob = createTextBlob(selected_str)
-        writeDataToClipboard(selected_blob).then(r => console.log("copied:" + selected_str))
-    }
-
-    function copyAll() {
-        console.clear()
+    function copy() {
         const selection = window.getSelection();
         if (!selection.rangeCount) return;
-
-        const range = selection.getRangeAt(0);
-        console.log('Selected elements:');
+        let range = selection.getRangeAt(0);
         let selected = range.cloneContents().children
-        // selected.forEach(e => console.log(e));
         let result = ""
         for (let selectedElement of selected) {
             result += selectedElement.outerHTML
         }
 
-        writeDataToClipboard(createHtmlBlob(result)).then(r => console.log("done"))
-        // navigator.clipboard.write([new ClipboardItem({'text/html': new Blob()})])
-        // console.log(typeof selected)
-        // let writable = new ClipboardItem({'text/html': new Blob([selected], {type: 'text/html'})})
-        // console.log('writable: ' + writable)
-        // // if (navigator.clipboard) {
-        // //     console.log('copying' + writable)
-        // //     navigator.clipboard.write([writable])
-        // // } else {
-        // console.log('copying2 ' + selected)
-        // document.execCommand('copy')
-        // }
+        writeDataToClipboard(createHtmlBlob(result)).then(
+            () => console.log("success")
+        ).catch(
+            () => console.log("failed")
+        )
     }
-    // document.addEventListener('mouseup', copyPlainText)
 
-    document.addEventListener('mouseup', copyAll);
+    document.addEventListener('mouseup', copy);
 
 })();
--- js/design_mode.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-    chrome.storage.local.get(['design_mode']).then(r => {
-        if (r.design_mode) {
-            document.designMode = 'on'
-        }
-    })
-}());
